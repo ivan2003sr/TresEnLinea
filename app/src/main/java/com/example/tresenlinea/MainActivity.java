1b22085a41df172524d0c12e583523a1d13ba3dd
ivan2003sr <ivan_2003sr@hotmail.com>
2020-09-23 16:37:28 -0300

    Inicio del Proyexto

---
+++ app/src/main/java/com/example/tresenlinea/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tresenlinea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
